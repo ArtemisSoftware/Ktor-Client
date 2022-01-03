c184718c83d0aa4023f1f81c74556f7aea7a4fda
gustavo <gustavo.isaac@gmail.com>
2022-01-03 16:36:00 +0000

    PostsService

---
+++ app/src/main/java/com/artemissoftware/ktorclient/data/remote/PostsService.kt
@@ -0,0 +1,32 @@
+package com.artemissoftware.ktorclient.data.remote
+
+import com.artemissoftware.ktorclient.data.remote.dto.PostRequest
+import com.artemissoftware.ktorclient.data.remote.dto.PostResponse
+import io.ktor.client.*
+import io.ktor.client.engine.android.*
+import io.ktor.client.features.json.*
+import io.ktor.client.features.json.serializer.*
+import io.ktor.client.features.logging.*
+
+interface PostsService {
+
+    suspend fun getPosts(): List<PostResponse>
+
+    suspend fun createPost(postRequest: PostRequest): PostResponse?
+
+
+    companion object {
+        fun create(): PostsService {
+            return PostsServiceImpl(
+                client = HttpClient(Android) {
+                    install(Logging) {
+                        level = LogLevel.ALL
+                    }
+                    install(JsonFeature) {
+                        serializer = KotlinxSerializer()
+                    }
+                }
+            )
+        }
+    }
+}
